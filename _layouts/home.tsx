8e3f2dcdafbd1efd79676a935a9f5f9558fad742
Henry Catalini Smith <henry@catalinismith.se>
2025-02-22 19:16:41 +0100

    Remove canonical link

--- _layouts/home.tsx
+++ _layouts/home.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import DefinitionList from "../_includes/definition-list"
+import React from "react";
+import DefinitionList from "../_includes/definition-list";
 
 export default function Home({
   title,
@@ -10,43 +10,26 @@ export default function Home({
     <html lang="en" dir="ltr">
       <head>
         <meta charSet="utf-8" />
-        <link
-          rel="canonical"
-          href="https://hen.cat/contrast/"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
-        <link
-          rel="stylesheet"
-          href="style.scss"
-        />
-        <title>
-          contrast
-        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="style.scss" />
+        <title>contrast</title>
       </head>
       <body>
         <main>
           <header>
-            <h1>
-              {title}
-            </h1>
-            <p>
-              {description}
-            </p>
+            <h1>{title}</h1>
+            <p>{description}</p>
           </header>
 
           <DefinitionList
-            items={collections.palettes.map(palette => ({
+            items={collections.palettes.map((palette) => ({
               termHref: `/${palette.fileSlug}`,
               termText: palette.data.name,
-              detailsText: palette.data.description
+              detailsText: palette.data.description,
             }))}
           />
         </main>
       </body>
     </html>
-  )
+  );
 }
-  
